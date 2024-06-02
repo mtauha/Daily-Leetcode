59abe41571a2fad0e77e43400c780b4391c81793
mtauha <111292199+mtauha@users.noreply.github.com>
2024-06-02 16:16:52 +0300

    Today's Problem

---
+++ reverse_string.py
@@ -0,0 +1,15 @@
+"""Description:
+    Write a function that reverses a string. The input string is given as an array of characters s.
+
+    You must do this by modifying the input array in-place with O(1) extra memory.    
+"""
+
+class Solution:
+    def reverseString(self, s: list[str]):
+        s[:] = s[::-1]
+
+
+sol = Solution()
+s = ["h", "e", "l", "l", "o"]
+sol.reverseString(s)
+print(s)
