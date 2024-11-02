7b699f107928a80bf929b0d495303e64dfdd11dc
Muhammad Tauha <muhammad.tauha@outlook.com>
2024-11-02 21:14:18 +0500

    Create circular-sentence.py

---
+++ circular-sentence.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        """
+            A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+            For example, "Hello World", "HELLO", "hello world hello world" are all sentences.
+            Words consist of only uppercase and lowercase English letters. Uppercase and lowercase English letters are considered different.
+            
+            A sentence is circular if:
+            
+            The last character of a word is equal to the first character of the next word.
+            The last character of the last word is equal to the first character of the first word.
+            For example, "leetcode exercises sound delightful", "eetcode", "leetcode eats soul" are all circular sentences. However, "Leetcode is cool", "happy Leetcode", "Leetcode" and "I like Leetcode" are not circular sentences.
+            
+            Given a string sentence, return true if it is circular. Otherwise, return false.
+        """
+        sentence = sentence.split()
+        last_char = sentence[0][-1]
+
+        for i in range(1, len(sentence)):
+            if last_char != sentence[i][0]:
+                return False
+            else:
+                last_char = sentence[i][-1]
+
+        if last_char == sentence[0][0]:
+            return True
+        
+        return False
+
