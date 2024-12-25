d3b4bb7952856f1619977ada7fc83fd221e9bd32
Muhammad Tauha <muhammad.tauha@outlook.com>
2024-12-25 11:16:42 +0500

    Create find-largest-value-in-each-tree-row.py

---
+++ find-largest-value-in-each-tree-row.py
@@ -0,0 +1,26 @@
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        """
+            Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+        """
+        if not root:
+            return []
+        
+        ans = []
+        queue = deque([root])
+        
+        while queue:
+            current_length = len(queue)
+            curr_max = float("-inf")
+            
+            for _ in range(current_length):
+                node = queue.popleft()
+                curr_max = max(curr_max, node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            ans.append(curr_max)
+        
+        return ans
