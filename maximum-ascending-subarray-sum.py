03a03871fb12a4744cdec67e3f2176dff8317fb1
Muhammad Tauha <muhammad.tauha@outlook.com>
2025-02-05 00:22:21 +0500

    Create maximum-ascending-subarray-sum.py

---
+++ maximum-ascending-subarray-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        maxSum = 0
+        currentSubarraySum = nums[0]
+
+        # Loop through the list starting from the second element
+        for currentIdx in range(1, len(nums)):
+            if nums[currentIdx] <= nums[currentIdx - 1]:
+                # If the current element is not greater than the previous one,
+                # update maxSum
+                maxSum = max(maxSum, currentSubarraySum)
+                # Reset the sum for the next ascending subarray
+                currentSubarraySum = 0
+            currentSubarraySum += nums[currentIdx]
+
+        # Final check after the loop ends to account for the last ascending
+        # subarray
+        return max(maxSum, currentSubarraySum)
