b9fe081500e3b54774f0fd1710edd0df947fc926
Muhammad Tauha <muhammad.tauha@outlook.com>
2025-06-09 15:39:01 +0500

    Update k-th-smallest-in-lexicographical-order.py

--- k-th-smallest-in-lexicographical-order.py
+++ k-th-smallest-in-lexicographical-order.py
@@ -1,27 +1,42 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
         """
-            Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n].
+            Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n]
         """
-        curr = 1
-        k -= 1
-        def countSteps(n, prefix1, prefix2):
-            steps = 0
+        def steps(prefix1, prefix2):
+            # Initialize step variable
+            step = 0
+
             while prefix1 <= n:
-                steps += min(n+1, prefix2) - prefix1
+                # Find the number of digits between prefix1 and prefix2 
+                # For Example, Number of digits between 1 and 2 bounded by n include all the numbers starting
+                # from 1 that are less than or equal to n.
+                step += min(n + 1, prefix2) - prefix1
+
+                # Move to the next levels
                 prefix1 *= 10
                 prefix2 *= 10
             
-            return steps
+            return step
+
+        
+        curr = 1
+        k -= 1
 
         while k > 0:
-            steps = countSteps(n, curr, curr+1)
-            if steps <= k:
+            step = steps(curr, curr + 1)
+
+            if step <= k:
+                # If steps(Numbers between current digit and next digit) is less than k, 
+                # Meaning we should skip this prefix and move to the next prefix 
+                # For Example if current prefix was 1 then we should move to 2. 
+                # To reduce computation we would decrement k by number of steps. 
+                # Current will be incremented.
                 curr += 1
-                k -= steps
+                k -= step
             else:
+                # We move to the next level until k is 0 to avoid extra computation
                 curr *= 10
                 k -= 1
-            
-        
+
         return curr
