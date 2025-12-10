955fb866d14e86d4bcbec6060616687055f17065
Muhammad Tauha <muhammad.tauha@outlook.com>
2025-12-11 02:58:59 +0500

    Add countPermutations function to calculate permutations
    Implement a function to count unlocking permutations based on complexity.

---
+++ count-the-number-of-computer-unlocking-permutations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPermutations(self, complexity: List[int]) -> int:
+        MOD = 10**9 + 7
+        n = len(complexity)
+
+        for i in range(1, n):
+            if complexity[i] <= complexity[0]:
+                return 0
+        
+        ans = 1
+        for i in range(2, n):
+            ans = (ans*i) % MOD
+        
+        return ans
