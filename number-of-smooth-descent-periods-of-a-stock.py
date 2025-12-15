a7a2ee7c35275e6e7c8f20b75ee27624c3296435
Muhammad Tauha <muhammad.tauha@outlook.com>
2025-12-15 22:50:31 +0500

    Implement method to count descent periods in stock prices

---
+++ number-of-smooth-descent-periods-of-a-stock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getDescentPeriods(self, prices: List[int]) -> int:
+        ans = 1
+        n = len(prices)
+        prev = 1
+
+        for i in range(1, n):
+            if prices[i] == prices[i - 1] - 1:
+                prev += 1
+            else:
+                prev = 1
+            ans += prev
+        return ans
