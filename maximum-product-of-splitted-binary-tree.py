c72380d33407009a88a2c368b27ea69fe6bf3fc7
Muhammad Tauha <muhammad.tauha@outlook.com>
2026-01-07 22:46:56 +0500

    Add maxProduct method for binary tree splitting
    Implement maxProduct function to calculate maximum product of split binary tree.

---
+++ maximum-product-of-splitted-binary-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxProduct(self, root: Optional[TreeNode]) -> int:
+        mod = 10**9 + 7
+
+        def dfs(node):
+            if not node:
+                return 0
+            
+            node.val += dfs(node.left) + dfs(node.right)
+            return node.val
+        
+        total = dfs(root)
+        queue = deque([root])
+        ans = 0
+
+        while queue:
+            node = queue.popleft()
+            if not node:
+                continue
+
+            curr_value = (total - node.val) * node.val
+            ans = max(ans, curr_value)
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        return ans % mod
