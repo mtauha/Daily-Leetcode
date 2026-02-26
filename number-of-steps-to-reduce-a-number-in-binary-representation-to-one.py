ec7f2c18636966528cbe5e2b5ab5819fe02a2ed1
Muhammad Tauha <muhammad.tauha@outlook.com>
2026-02-26 21:58:32 +0500

    Implement solution to reduce binary number to one

---
+++ number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numSteps(self, s: str) -> int:
+        n = int(s, 2)
+        count = 0
+
+        while n != 1:
+            if n % 2 == 0:
+                n //= 2
+            else:
+                n += 1
+            count += 1
+        
+        return count
