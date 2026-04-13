4a8497ca1bdc0a03bad84665687172955381eb0a
Muhammad Tauha <muhammad.tauha@outlook.com>
2026-04-13 21:04:19 +0500

    Implement function to find minimum distance to target

---
+++ minimum-distance-to-the-target-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        ans = 10001
+
+        for i, num in enumerate(nums):
+            if num == target:
+                ans = min(ans, abs(i - start))
+        
+        return ans
