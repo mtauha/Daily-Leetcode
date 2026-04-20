6e0ed647ce4a0c56cb04d05fa49c2fcd556de9de
Muhammad Tauha <muhammad.tauha@outlook.com>
2026-04-20 22:01:24 +0500

    Implement solution for maxDistance of houses

---
+++ two-furthest-houses-with-different-colors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxDistance(self, A: List[int]) -> int:
+        n = len(A)
+        left, right = 0, n - 1
+
+        # Find the first mismatch from the left side with the last element
+        for i in range(n):
+            if A[i] ^ A[-1]:
+                left = i
+                break
+
+        # Find the first mismatch from the right side with first element
+        for i in range(n - 1, -1, -1):
+            if A[i] ^ A[0]:
+                right = i
+                break
+
+        return max(n - 1 - left, right)
